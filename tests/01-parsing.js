8c866e0f224942f1cb15212d79d8dc3cd54e90be
Melvin Tiong <cy.tiong@gmail.com>
2017-07-18 22:18:15 +0800

    Spec basic unquoted assignment.

--- tests/01-parsing.js
+++ tests/01-parsing.js
@@ -4,6 +4,15 @@ const nvar = require('../lib');
 // Helper to initialise new instances for each test.
 function nv(source) {return nvar({source, target: Object.create(null)});}
 
+test('Unquoted values', function (t) {
+  t.deepEqual(nv('toast=kaya'), {toast: 'kaya'}, 'Handles basic unquoted assignment.');
+  t.deepEqual(nv('toast=kaya nope'), {toast: 'kaya'}, 'Ignores non-assignments.');
+  t.deepEqual(nv('toast=kaya eggs=hardboiled nope nope nope'), {toast: 'kaya', eggs: 'hardboiled'}, 'Accepts multiple assignments at the start of a single line.');
+  t.deepEqual(nv('toast=kaya nope sorry=darling'), {toast: 'kaya'}, 'Ignores assignment-like statements once a non-assignment has been encountered.');
+
+  t.end();
+});
+
 // Canon: https://www.gnu.org/software/bash/manual/html_node/Single-Quotes.html
 test('Escaping with single-quotes', function (t) {
   t.deepEqual(nv("toast='kaya'"), {toast: 'kaya'}, 'Does not include surrounding quotes in final value.');

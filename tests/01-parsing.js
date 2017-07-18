b7d40b5b9de5a8a791f666cc9412e2cfd726b038
Melvin Tiong <cy.tiong@gmail.com>
2017-07-18 22:17:55 +0800

    Tweak test boilerplate.

--- tests/01-parsing.js
+++ tests/01-parsing.js
@@ -1,9 +1,10 @@
 const test = require('tape');
-const nvar = require('nvar');
+const nvar = require('../lib');
 
-// Create new instances as required for each test.
+// Helper to initialise new instances for each test.
 function nv(source) {return nvar({source, target: Object.create(null)});}
 
+// Canon: https://www.gnu.org/software/bash/manual/html_node/Single-Quotes.html
 test('Escaping with single-quotes', function (t) {
   t.deepEqual(nv("toast='kaya'"), {toast: 'kaya'}, 'Does not include surrounding quotes in final value.');
   t.deepEqual(nv("toast='  kaya toast\t'"), {toast: '  kaya toast\t'}, 'Treats spaces and tabs literally.');
